9013c5e49b25aaba67f3a0fe04b3100cff6d3c88
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-28 00:28:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number (array, name)
     puts "Welcome, #{name}. You are number #{array.length} in line."
 end 
 
-def now_serving 
+def now_serving(katz_deli)
   if  katz_deli == []
     puts "There is nobody waiting to be served!"
   else
